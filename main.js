96cc1617cdc8f15914ae345d0b8d8ed0f2fac311
calebslade <caleb.slade10@gmail.com>
2024-07-22 18:17:29 -0600

    project finish

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log('hello world')
+import { crawlPage } from "./crawl.js"
+import { printReport } from "./report.js"
+
+
+async function main(){
+    if (process.argv.length < 3){
+        console.log('no website given')
+        return
+    }
+    if (process.argv.length > 3){
+        console.log('too many args')
+        return
+    }
+    const baseURL = process.argv[2]
+
+    console.log(`starting crawl on ${baseURL}`)
+
+    const pages = await crawlPage(baseURL)
+
+    printReport(pages)
+}
+
+main()
